71ee3f66a0e839c4584b714dae7c1c7aa66121d8
vagrant <vagrant@homestead>
2024-07-16 08:57:10 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,11 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');;
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', UsersController::class);
+// Route::get('/users', 'UsersController@index')->name('users.index');
+// Route::get('/users/create', 'UsersController@create')->name('users.create');
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::post('/users', 'UsersController@store')->name('users.store');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
