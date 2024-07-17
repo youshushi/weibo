e4093ebfc7359989770309405f954394f3b11c9a
vagrant <vagrant@homestead>
2024-07-17 09:04:04 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
         }
         return;
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '成功退出');
+        return redirect('login');
+    }
 }
