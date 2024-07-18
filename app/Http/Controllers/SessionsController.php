9877805a32a2658437a2f6a8b9d3676cf0b054a2
vagrant <vagrant@homestead>
2024-07-18 09:19:41 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,10 +28,16 @@ class SessionsController extends Controller
         ]);
 
         if (Auth::attempt($credential, $request->has('remember'))) {
-            session()->flash('success', 'welcome');
-            return redirect()->route('users.show', [Auth::user()]);
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', [Auth::user()]);
+                return redirect()->intended($fallback);
+            } else {
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
         } else {
-            session()->flash('danger', 'sorry');
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
         return;
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -33,6 +34,13 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($user) {
+            $user->activation_token = Str::random(10);
+        });
+    }
     /**
      * The attributes that should be cast.
      *
