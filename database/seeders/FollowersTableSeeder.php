1f7cd63ad26c56f40ba5e86831c2e3f8a8506791
vagrant <vagrant@homestead>
2024-07-22 07:43:35 +0000

    社交统计信息

---
+++ database/seeders/FollowersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class FollowersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+        $user = $users->first();
+        $user_id = $user->id;
+
+        $followers = $users->slice(1);
+        $follower_ids = $followers->pluck('id')->toArray();
+
+        $user->follow($follower_ids);
+
+        foreach ($followers as $follower) {
+            $follower->follow($user_id);
+        }
+    }
+}
